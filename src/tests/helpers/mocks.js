21cd7b18de8025e95209a859f3a6fb7f9b9bd687
eduardoSousa <dudus.sousa4002@gmail.com>
2023-01-11 19:23:12 -0300

    requisito 4 concluido

---
+++ src/tests/helpers/mocks.js
@@ -0,0 +1,6 @@
+const obj = { response_code: 0,
+  response_message: 'Token Generated Successfully!',
+  token: '508b87ccf72cab7115abaace5a4459ffb36f42479a25614413936c8ad8b5deea',
+};
+
+export default obj;
