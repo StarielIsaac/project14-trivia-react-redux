c3256d8f5be491be6d32893b1e5c47f7705c00b2
Jean Alves <j_alves_19@hotmail.com>
2023-01-12 14:22:51 -0400

    req 6

--- src/api/requestTrivia.js
+++ src/api/requestTrivia.js
@@ -1,7 +1,8 @@
-const requestToken = async () => {
+export const requestToken = async () => {
   const response = await fetch('https://opentdb.com/api_token.php?command=request');
   const data = await response.json();
   return data;
 };
 
-export default requestToken;
+export const validationToken = (token) => fetch(`https://opentdb.com/api.php?amount=5&token=${token}`)
+  .then((response) => response.json());
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { validationToken } from '../api/requestTrivia';
 
 class Game extends Component {
+  componentDidMount() {
+    const obj = JSON.parse(localStorage.getItem('token'));
+    console.log(obj);
+  }
+
   render() {
     return (
       <div>
